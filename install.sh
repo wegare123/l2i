e1ecb942fae141ec3298ccdd48a3c4045d6dae71
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-02-12 13:43:01 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 #l2i (Wegare)
 printf 'ctrl+c' | crontab -e > /dev/null
 opkg update && opkg install xl2tpd strongswan-default fping
+/etc/init.d/ipsec stop 2>/dev/null
+/etc/init.d/xl2tpd stop 2>/dev/null
+/etc/init.d/ipsec disable 2>/dev/null
+/etc/init.d/xl2tpd disable 2>/dev/null
+killall -q charon
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/l2i/main/l2i.sh" -O /usr/bin/l2i
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/l2i/main/autorekonek-l2i.sh" -O /usr/bin/autorekonek-l2i
 chmod +x /usr/bin/l2i
